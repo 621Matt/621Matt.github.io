71cd95d7816b6cb9bb493baa6c0c8114c70c9eda
[621Matt] <144735888+621Matt@users.noreply.github.com>
2025-04-28 05:26:28 -0400

    Updated client project

--- itis3135/project/scripts/active_page.js
+++ itis3135/project/scripts/active_page.js
@@ -1,21 +1,6 @@
 let link = document.getElementsByTagName("a");
 let activeIndex = localStorage.getItem("activeIndex");
 
-for (let i = 0; i < link.length; i++) {
-  // Applies active class on saved index
-  if (String(i) === activeIndex) {
-    link[i].classList.add("active");
-  }
-
-  link[i].addEventListener("click", () => {
-    if (link[i].classList.contains("active")) {
-      link[i].classList.remove("active");
-    } else {
-      link[i].classList.add("active");
-      localStorage.setItem("activeIndex", i);
-    }
-  });
-}
 
 for (let i = 0; i < link.length; i++) {
     // Applies active class on saved index
